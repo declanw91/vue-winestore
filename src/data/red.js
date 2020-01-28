ff60bd6eb1850771058c695ebdccb34f93eb27ad
Declan <dwright1991@gmail.com>
2020-01-28 19:03:47 +0000

    data updates for build for prod

--- src/data/red.js
+++ src/data/red.js
@@ -1,130 +1,130 @@
 export default [
     {
-        "Id": "0",
-        "Name": "Chateau Greaysac",
-        "Colour": "Red",
-        "Price": "19.99",
-        "Year": "2004",
-        "Region": "Burgundy"
+        'Id': '0',
+        'Name': 'Chateau Greaysac',
+        'Colour': 'Red',
+        'Price': '19.99',
+        'Year': '2004',
+        'Region': 'Burgundy'
     },
     {
-        "Id": "1",
-        "Name": "Chateau Greaysac",
-        "Colour": "Red",
-        "Price": "19.99",
-        "Year": "2004",
-        "Region": "Bordeaux"
+        'Id': '1',
+        'Name': 'Chateau Greaysac',
+        'Colour': 'Red',
+        'Price': '19.99',
+        'Year': '2004',
+        'Region': 'Bordeaux'
     },
     {
-        "Id": "2",
-        "Name": "Chateau Greaysac",
-        "Colour": "Red",
-        "Price": "19.99",
-        "Year": "2004",
-        "Region": "Rhone"
+        'Id': '2',
+        'Name': 'Chateau Greaysac',
+        'Colour': 'Red',
+        'Price': '19.99',
+        'Year': '2004',
+        'Region': 'Rhone'
     },
     {
-        "Id": "3",
-        "Name": "Chateau Greaysac",
-        "Colour": "Red",
-        "Price": "19.99",
-        "Year": "2004",
-        "Region": "Alsace"
+        'Id': '3',
+        'Name': 'Chateau Greaysac',
+        'Colour': 'Red',
+        'Price': '19.99',
+        'Year': '2004',
+        'Region': 'Alsace'
     },
     {
-        "Id": "4",
-        "Name": "Chateau Dassault St. Emilion Grand Cru",
-        "Colour": "Red",
-        "Price": "34.89",
-        "Year": "2003",
-        "Region": "Burgundy"
+        'Id': '4',
+        'Name': 'Chateau Dassault St. Emilion Grand Cru',
+        'Colour': 'Red',
+        'Price': '34.89',
+        'Year': '2003',
+        'Region': 'Burgundy'
     },
     {
-        "Id": "5",
-        "Name": "Chateau Dassault St. Emilion Grand Cru",
-        "Colour": "Red",
-        "Price": "34.89",
-        "Year": "2003",
-        "Region": "Bordeaux"
+        'Id': '5',
+        'Name': 'Chateau Dassault St. Emilion Grand Cru',
+        'Colour': 'Red',
+        'Price': '34.89',
+        'Year': '2003',
+        'Region': 'Bordeaux'
     },
     {
-        "Id": "6",
-        "Name": "Chateau Dassault St. Emilion Grand Cru",
-        "Colour": "Red",
-        "Price": "34.89",
-        "Year": "2003",
-        "Region": "Rhone"
+        'Id': '6',
+        'Name': 'Chateau Dassault St. Emilion Grand Cru',
+        'Colour': 'Red',
+        'Price': '34.89',
+        'Year': '2003',
+        'Region': 'Rhone'
     },
     {
-        "Id": "7",
-        "Name": "Chateau Dassault St. Emilion Grand Cru",
-        "Colour": "Red",
-        "Price": "34.89",
-        "Year": "2003",
-        "Region": "Alsace"
+        'Id': '7',
+        'Name': 'Chateau Dassault St. Emilion Grand Cru',
+        'Colour': 'Red',
+        'Price': '34.89',
+        'Year': '2003',
+        'Region': 'Alsace'
     },
     {
-        "Id": "8",
-        "Name": "Chateau La Freynelle Rouge",
-        "Colour": "Red",
-        "Price": "16.11",
-        "Year": "2006",
-        "Region": "Burgundy"
+        'Id': '8',
+        'Name': 'Chateau La Freynelle Rouge',
+        'Colour': 'Red',
+        'Price': '16.11',
+        'Year': '2006',
+        'Region': 'Burgundy'
     },
     {
-        "Id": "9",
-        "Name": "Chateau La Freynelle Rouge",
-        "Colour": "Red",
-        "Price": "16.11",
-        "Year": "2006",
-        "Region": "Bordeaux"
+        'Id': '9',
+        'Name': 'Chateau La Freynelle Rouge',
+        'Colour': 'Red',
+        'Price': '16.11',
+        'Year': '2006',
+        'Region': 'Bordeaux'
     },
     {
-        "Id": "10",
-        "Name": "Chateau La Freynelle Rouge",
-        "Colour": "Red",
-        "Price": "16.11",
-        "Year": "2006",
-        "Region": "Rhone"
+        'Id': '10',
+        'Name': 'Chateau La Freynelle Rouge',
+        'Colour': 'Red',
+        'Price': '16.11',
+        'Year': '2006',
+        'Region': 'Rhone'
     },
     {
-        "Id": "11",
-        "Name": "Chateau La Freynelle Rouge",
-        "Colour": "Red",
-        "Price": "16.11",
-        "Year": "2006",
-        "Region": "Alsace"
+        'Id': '11',
+        'Name': 'Chateau La Freynelle Rouge',
+        'Colour': 'Red',
+        'Price': '16.11',
+        'Year': '2006',
+        'Region': 'Alsace'
     },
     {
-        "Id": "12",
-        "Name": "Joseph Drouhin Bourgogne Vero Pinot Noir",
-        "Colour": "Red",
-        "Price": "14.89",
-        "Year": "2004",
-        "Region": "Burgundy"
+        'Id': '12',
+        'Name': 'Joseph Drouhin Bourgogne Vero Pinot Noir',
+        'Colour': 'Red',
+        'Price': '14.89',
+        'Year': '2004',
+        'Region': 'Burgundy'
     },
     {
-        "Id": "13",
-        "Name": "Joseph Drouhin Bourgogne Vero Pinot Noir",
-        "Colour": "Red",
-        "Price": "14.89",
-        "Year": "2004",
-        "Region": "Bordeaux"
+        'Id': '13',
+        'Name': 'Joseph Drouhin Bourgogne Vero Pinot Noir',
+        'Colour': 'Red',
+        'Price': '14.89',
+        'Year': '2004',
+        'Region': 'Bordeaux'
     },
     {
-        "Id": "14",
-        "Name": "Joseph Drouhin Bourgogne Vero Pinot Noir",
-        "Colour": "Red",
-        "Price": "14.89",
-        "Year": "2004",
-        "Region": "Rhone"
+        'Id': '14',
+        'Name': 'Joseph Drouhin Bourgogne Vero Pinot Noir',
+        'Colour': 'Red',
+        'Price': '14.89',
+        'Year': '2004',
+        'Region': 'Rhone'
     },
     {
-        "Id": "15",
-        "Name": "Joseph Drouhin Bourgogne Vero Pinot Noir",
-        "Colour": "Red",
-        "Price": "14.89",
-        "Year": "2004",
-        "Region": "Alsace"
+        'Id': '15',
+        'Name': 'Joseph Drouhin Bourgogne Vero Pinot Noir',
+        'Colour': 'Red',
+        'Price': '14.89',
+        'Year': '2004',
+        'Region': 'Alsace'
     }
 ]
